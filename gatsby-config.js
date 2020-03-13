61530b4f6c0b2d78246ccb8e3f1a6ed3ad5d96eb
Nicolas Chenet <nicolas.chenet@datadoghq.com>
2020-03-13 16:55:38 +0100

    📈 Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,89 +1,89 @@
 module.exports = {
-  siteMetadata: {
-    title: `Tech thoughts`,
-    author: `Nicolas Chenet`,
-    description: `Just thoughts`,
-    siteUrl: `https://adoring-lumiere-60e84d.netlify.com/`,
-    social: {
-      twitter: `nicolaschenet`,
+    siteMetadata: {
+        title: `Tech thoughts`,
+        author: `Nicolas Chenet`,
+        description: `Just thoughts`,
+        siteUrl: `https://adoring-lumiere-60e84d.netlify.com/`,
+        social: {
+            twitter: `nicolaschenet`,
+        },
     },
-  },
-  plugins: [
-    `gatsby-plugin-netlify-cms`,
-    `gatsby-plugin-styled-components`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-feed-mdx`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/blog`,
-        name: `blog`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        extensions: [".mdx", ".md"],
-        gatsbyRemarkPlugins: [
-          {
-            resolve: `gatsby-remark-images`,
+    plugins: [
+        `gatsby-plugin-netlify-cms`,
+        `gatsby-plugin-styled-components`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        `gatsby-plugin-offline`,
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-feed-mdx`,
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              maxWidth: 590,
+                path: `${__dirname}/content/blog`,
+                name: `blog`,
             },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
+                path: `${__dirname}/content/assets`,
+                name: `assets`,
             },
-          },
-          {
-            resolve: `gatsby-remark-vscode`,
-          },
-          {
-            resolve: `gatsby-remark-copy-linked-files`,
-          },
-          {
-            resolve: `gatsby-remark-smartypants`,
-          },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // edit below
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Blog`,
-        short_name: `Blog`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        // edit below
-        icon: `content/assets/gatsby-icon.png`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-  ],
+        },
+        {
+            resolve: `gatsby-plugin-mdx`,
+            options: {
+                extensions: ['.mdx', '.md'],
+                gatsbyRemarkPlugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 590,
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-responsive-iframe`,
+                        options: {
+                            wrapperStyle: `margin-bottom: 1.0725rem`,
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-vscode`,
+                    },
+                    {
+                        resolve: `gatsby-remark-copy-linked-files`,
+                    },
+                    {
+                        resolve: `gatsby-remark-smartypants`,
+                    },
+                ],
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                // edit below
+                trackingId: `UA-160631327-1`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Blog`,
+                short_name: `Blog`,
+                start_url: `/`,
+                background_color: `#ffffff`,
+                theme_color: `#663399`,
+                display: `minimal-ui`,
+                // edit below
+                icon: `content/assets/gatsby-icon.png`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-typography`,
+            options: {
+                pathToConfigModule: `src/utils/typography`,
+            },
+        },
+    ],
 }
