2d1a9c042aff2d36a46aa186e415ad15caa038c2
Nicolas Chenet <nicolas.chenet@datadoghq.com>
2020-03-16 19:45:47 +0100

    ✨ Add resume content

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,13 @@ module.exports = {
             },
         },
         {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/content/resume`,
+                name: `resume`,
+            },
+        },
+        {
             resolve: `gatsby-plugin-mdx`,
             options: {
                 extensions: ['.mdx', '.md'],
